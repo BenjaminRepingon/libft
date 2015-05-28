1b3c7085b7c950ae1049d5561ae8d9a74ba16d1c
BenjaminRepingon <repingonbenjamin@gmail.com>
2015-05-28 15:45:59 +0200

    fix ft_atof

--- convert/ft_atof.c
+++ convert/ft_atof.c
@@ -6,7 +6,7 @@
 /*   By: rbenjami <rbenjami@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/26 14:01:54 by rbenjami          #+#    #+#             */
-/*   Updated: 2015/05/26 14:28:49 by rbenjami         ###   ########.fr       */
+/*   Updated: 2015/05/28 15:44:17 by rbenjami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,23 @@ float			ft_atof(const char *str)
 	int		i;
 	float	value;
 	float	divisor;
+	int		negative;
 
 	tmp = (char *)str;
 	value = ft_atoi(str);
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '+' || (negative = (*str == '-')))
+		str++;
 	i = 0;
 	while (tmp[i] && tmp[i] != '.')
 		i++;
-	i++;
-	tmp += i;
+	tmp += i + 1;
 	i = 0;
-	divisor = 10;
-	while (tmp[i] && (tmp[i] <= '9') && (tmp[i] > '0'))
+	divisor = (negative) ? -10 : 10;
+	while (tmp[i] && (tmp[i] <= '9') && (tmp[i] >= '0'))
 	{
-		if (value > 0)
-			value += (float)(tmp[i] - '0') / divisor;
-		else
-			value -= (float)(tmp[i] - '0') / divisor;
+		value += (float)(tmp[i] - '0') / divisor;
 		divisor *= 10;
 		i++;
 	}
